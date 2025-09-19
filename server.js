8286a00922f756dfc710f84d2e2ae155b8d92c9e
Kittisak064 <nongbasthongpool@gmail.com>
2025-09-19 19:36:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-async function testSheet() {
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: Buffer.from(process.env.GOOGLE_PRIVATE_KEY_BASE64, "base64").toString("utf-8"),
-  });
-  await doc.loadInfo();
-  console.log("✅ Google Sheet title:", doc.title);
-}
-testSheet();
+==> Deploying...
+==> Running 'npm start'
+> line-bot@1.0.0 start
+> node server.js
+file:///opt/render/project/src/server.js:2
+  await doc.useServiceAccountAuth({
+  ^
+ReferenceError: doc is not defined
+    at testSheet (file:///opt/render/project/src/server.js:2:3)
+    at file:///opt/render/project/src/server.js:9:1
+    at ModuleJob.run (node:internal/modules/esm/module_job:274:25)
+    at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:644:26)
+    at async asyncRunEntryPointWithESMLoader (node:internal/modules/run_main:117:5)
+Node.js v22.16.0
+==> Exited with status 1
+==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
+==> Running 'npm start'
+> line-bot@1.0.0 start
+> node server.js
+file:///opt/render/project/src/server.js:2
+  await doc.useServiceAccountAuth({
+  ^
+ReferenceError: doc is not defined
+    at testSheet (file:///opt/render/project/src/server.js:2:3)
+    at file:///opt/render/project/src/server.js:9:1
+    at ModuleJob.run (node:internal/modules/esm/module_job:274:25)
+    at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:644:26)
+    at async asyncRunEntryPointWithESMLoader (node:internal/modules/run_main:117:5)
+Node.js v22.16.0
